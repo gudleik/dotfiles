e77588da896a816f0f2f52f43d8ed4d3330efcc1
Gudleik Rasch <gudleik@gmail.com>
2011-09-13 15:54:10 +0200

    functions for creating servers

--- dotfiles/.knife_helpers.sh
+++ dotfiles/.knife_helpers.sh
@@ -6,3 +6,16 @@ function knife-ssh() {
 
   ssh ubuntu@$host
 }
+
+function knife-create-server() {
+  name=$1;
+  flavor=$2;
+  ami=$3;
+
+  knife ec2 server create -N ${name} -f ${flavor} -I ${ami} -S default -x ubuntu -r "role[base]" -i ~/.ssh/aws-skalar-default.pem --region eu-west-1 -Z eu-west-1a
+}
+
+function knife-create-medium() {
+  name=$1;
+  knife-create-server $name "c1.medium" "ami-359ea941"
+}
