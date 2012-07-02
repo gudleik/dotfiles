6ec0ec4ae6a818c3b2b9ea8cbde5411d5932a375
Gudleik Rasch <gudleik@gmail.com>
2012-07-02 11:48:05 +0200

    alias to install default gems

--- dotfiles/in+.oh-my-zsh/custom/aliases.zsh
+++ dotfiles/in+.oh-my-zsh/custom/aliases.zsh
@@ -6,3 +6,7 @@ alias grep='grep --color=auto'
 alias n='nc -vz'
 
 alias deploy='cd ~/code/skalar/DevOps/Deploy'
+
+function install-default-gems() {
+  gem install bundler dotty git-smart pry pry-doc pry-editline awesome_print hirb
+}
