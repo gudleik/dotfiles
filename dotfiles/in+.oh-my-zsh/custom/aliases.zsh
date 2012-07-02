5c09d86aeabb4c8e4155cbdc7cbaf7a2118328b5
Gudleik Rasch <gudleik@gmail.com>
2012-07-02 08:28:29 +0200

    aliases

--- dotfiles/in+.oh-my-zsh/custom/aliases.zsh
+++ dotfiles/in+.oh-my-zsh/custom/aliases.zsh
@@ -4,3 +4,5 @@ alias la='ls -la'
 alias l='less'
 alias grep='grep --color=auto'
 alias n='nc -vz'
+
+alias deploy='cd ~/code/skalar/DevOps/Deploy'
--- dotfiles/in+.oh-my-zsh/custom/bundler-aliases.zsh
+++ dotfiles/in+.oh-my-zsh/custom/bundler-aliases.zsh
@@ -1,4 +1,5 @@
 # Bundler aliases 
 alias b='bundle' 
-alias bi="b install --path vendor" 
-alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore" 
+alias bi="b install --path ~/code/gems" 
+#alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore" 
+alias binit="bi && b package"
