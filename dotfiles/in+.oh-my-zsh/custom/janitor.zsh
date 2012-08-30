4a0545f023d8884fe9832e3d98ca9d1632482ff3
Gudleik Rasch <gudleik@gmail.com>
2012-08-30 21:18:24 +0200

    update rbenv plugins (update-stuff)

--- dotfiles/in+.oh-my-zsh/custom/janitor.zsh
+++ dotfiles/in+.oh-my-zsh/custom/janitor.zsh
@@ -5,5 +5,17 @@ function update-stuff() {
   dotty update vimconfig
   dotty update_submodules vimconfig
 
+  update-rbenv-plugins
+
   brew outdated
 }
+
+function update-rbenv-plugins() {
+  pwd=$(pwd) 
+  for plugin in ~/.rbenv/plugins/*; do
+    cd $plugin
+    git pull -q origin master
+  done
+
+  cd $pwd
+} 
